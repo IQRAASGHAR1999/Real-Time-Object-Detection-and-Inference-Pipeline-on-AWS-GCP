a09cd8f511700422375c56c05e7bdf9d83884706
Iqra Asghar <114947104+IQRAASGHAR1999@users.noreply.github.com>
2025-09-25 22:50:38 +0500

    Create train.py

---
+++ src/train.py
@@ -0,0 +1,7 @@
+"""
+For custom training, see Ultralytics YOLOv8 docs:
+https://docs.ultralytics.com/
+Example:
+    yolo train data=data.yaml model=yolov8n.pt epochs=50 imgsz=640
+"""
+print("Please refer to Ultralytics YOLOv8 documentation for custom training.")
